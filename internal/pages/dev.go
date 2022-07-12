e764474c690fabc1a9eeec9330c567e93c165f67
HJin.me <hjin.me@qq.com>
2022-07-12 18:33:04 +0800

    fix: 修复一下自己的bug

--- internal/pages/dev.go
+++ internal/pages/dev.go
@@ -3,6 +3,7 @@ package pages
 import (
 	"context"
 	rawHttp "net/http"
+	"net/url"
 
 	"github.com/lunzi/aacs/internal/biz"
 	"github.com/pkg/errors"
@@ -16,6 +17,7 @@ func DevPage(ident biz.IdentRepo, tpRepo biz.ThirdPartyRepo) PageHandler {
 		if tk == "" {
 			return errors.New("未授权的访问")
 		}
+		expAt := r.URL.Query().Get(biz.NameExpiredAt)
 		sub, err := ident.VerifyToken(ctx, tk)
 		if err != nil {
 			return errors.WithMessage(err, "未授权的访问")
@@ -27,7 +29,18 @@ func DevPage(ident biz.IdentRepo, tpRepo biz.ThirdPartyRepo) PageHandler {
 		if !appInfo.DevMode {
 			return errors.New("非开发模式")
 		}
-		return Render(ctx, "dev", w, appInfo)
+		u, err := url.Parse(appInfo.CallbackUrl)
+		if err != nil {
+			return errors.WithMessage(err, "应用回调地址不合法")
+		}
+		values := u.Query()
+		values.Set(biz.NameTk, tk)
+		values.Set(biz.NameExpiredAt, expAt)
+		u.RawQuery = values.Encode()
+
+		return Render(ctx, "dev", w, struct {
+			CallbackUrl string `json:"callbackUrl"`
+		}{CallbackUrl: u.String()})
 
 	}
 }
